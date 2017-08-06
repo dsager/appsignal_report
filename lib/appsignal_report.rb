501989872a133e225113fec73b153e9ac5a3c776
Daniel Sager <daniel@sager1.de>
2017-08-06 18:45:15 +0200

    Extract generic stuff to report base class

---
+++ lib/appsignal_report.rb
@@ -0,0 +1,53 @@
+class AppsignalReport
+  attr_reader :api_token, :app_id
+
+  # @param [String] api_token API token, find it here:
+  #                           <https://appsignal.com/users/edit>
+  # @param [String] app_id    Application ID, visible in the URL when your
+  #                           application is opened on Appsignal.com
+  def initialize(api_token:, app_id:)
+    @api_token = api_token
+    @app_id = app_id
+  end
+
+  def print
+    puts "\nAppSignal - #{self.class.name.sub('Appsignal', '')}\n"
+    report = generate
+    report.each { |key, value| puts format('%30s: %s', key, value) }
+    puts "\n\n"
+  end
+
+  # To be defined by subclass
+  # @return [Hash]
+  def generate; end
+
+  private
+
+  # @param [Array[Hash]] data
+  # @param [Symbol] field
+  # @return [Float]
+  def get_average(data, field)
+    values = data.map { |row| row[field] }
+    values.inject(0, :+).fdiv(values.size)
+  end
+
+  # @return [String]
+  def base_uri
+    "https://appsignal.com/api/#{app_id}"
+  end
+
+  # @return [Hash]
+  def perform_api_request(uri)
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+
+    response = http.request(
+      Net::HTTP::Get.new(uri, 'Content-Type' => 'application/json')
+    )
+    if response.is_a? Net::HTTPSuccess
+      JSON.parse(response.body, symbolize_names: true)
+    else
+      raise StandardError, "[API ERROR] #{response.code} - #{response.message}"
+    end
+  end
+end
--- lib/appsignal_weekly_report.rb
+++ lib/appsignal_weekly_report.rb
@@ -1,39 +1,22 @@
-class AppsignalWeeklyReport
-  attr_reader :api_token, :app_id, :report
-
-  # @param [String] api_token API token, find it here:
-  #                           <https://appsignal.com/users/edit>
-  # @param [String] app_id    Application ID, visible in the URL when your
-  #                           application is opened on Appsignal.com
-  def initialize(api_token:, app_id:)
-    @api_token = api_token
-    @app_id = app_id
-    @report = nil
-  end
-
-  def print
-    generate
-    puts '', ' ## AppSignal - Weekly Report ##', ''
-    report.each do |key, value|
-      puts format('%25s: %s', key, value)
-    end
+require_relative 'appsignal_report'
+#
+# Weekly Report
+#
+# Calculates the average values over the last week for a few metrics:
+# - Error Rate
+# - Response Time
+# - Hourly Throughput
+#
+class AppsignalWeeklyReport < AppsignalReport
+  def generate
+    parse_api_response(perform_api_request(uri))
   end
 
   private
 
-  def generate
-    @report = summarize(
-      perform_api_request(
-        uri(
-          token: api_token,
-          timeframe: :week,
-          'fields[]': %i(mean count ex_rate)
-        )
-      )
-    )
-  end
-
-  def summarize(data)
+  # @return [Hash]
+  # @param [Object] data
+  def parse_api_response(data)
     {
       from: data[:from],
       to: data[:to],
@@ -43,30 +26,13 @@ class AppsignalWeeklyReport
     }
   end
 
-  def get_average(data, field)
-    values = data.map { |row| row[field] }
-    values.inject(0, :+).fdiv(values.size)
-  end
-
-  # @param [Hash] params
-  def uri(params = {})
-    query = URI.encode_www_form(params)
-    URI("https://appsignal.com/api/#{app_id}/graphs.json?#{query}")
-  end
-
-  # @param [URI] uri
-  # @return [Hash]
-  def perform_api_request(uri)
-    http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = true
-
-    response = http.request(
-      Net::HTTP::Get.new(uri, 'Content-Type' => 'application/json')
+  # @return [URI]
+  def uri
+    query = URI.encode_www_form(
+      token: api_token,
+      timeframe: :week,
+      'fields[]': %i(mean count ex_rate)
     )
-    if response.is_a? Net::HTTPSuccess
-      JSON.parse(response.body, symbolize_names: true)
-    else
-      raise StandardError, "[API ERROR] #{response.code} - #{response.message}"
-    end
+    URI("#{base_uri}/graphs.json?#{query}")
   end
 end
